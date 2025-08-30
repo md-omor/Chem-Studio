68854583f89c187c464e1b83f9ef1b32242b0c37
Md Omor <developeromor@gmail.com>
2025-08-30 16:05:14 +0600

    add scroll to top

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import AiAssistant from "@/pages/ai-assistant";
 import Home from "@/pages/home";
 import JoinUs from "@/pages/join-us";
 import KnowledgeCenter from "@/pages/knowledge-center";
@@ -11,11 +10,14 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Route, Switch } from "wouter";
 import Footer1 from "./components/Footer1";
 import { Navigation1 } from "./components/Navigation1";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { queryClient } from "./lib/queryClient";
+import AiAssistant from "./pages/ai-assistant-new";
 
 function Router() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navigation1 />
       <main className="flex-1">
         <Switch>
